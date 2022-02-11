9a1124a25ff060008dc2dfddc4895d2aac1c9b83
renan04lima <renan04lima@outlook.com>
2022-02-11 11:16:45 -0300

    chore: add socket.io and express

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,1 +1,20 @@
 import './config/module.alias'
+import { app } from '@/main/config/app'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+
+const server = createServer(app)
+
+app.get('/', (req, res) => {
+  res.json({ message: 'api works!' })
+})
+
+const io = new Server(server)
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+})
+
+server.listen(3333, () => console.log('running on port 3333'))
